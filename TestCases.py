e3f8f88e8ea43a9854fa3b31e544eaacde7f9b11
bgreni <42788181+bgreni@users.noreply.github.com>
2019-03-03 18:28:37 -0700

    Progress commit

--- TestCases.py
+++ TestCases.py
@@ -7,17 +7,80 @@ class Tests:
     
     def run(self):
         self.testGetAllPapers()
+        self.testGetPaperReviewers()
+        self.testPotentialReviewers()
+        self.testInsert()
     
     def testGetPaperReviewers(self):
-        pass
+        testPassed = True
+        expected1 = [('Minnie@Email',), ('Donald@Email',), ('Mickey@Email',)]
+        expected2 = [("Anakin@Email",),("Darth@Email",),("Pluto@Email",)]
+        expected3 = [("C3P0@Email",),("R2D2@Email",),("Tom@Email",)]
+        # case 1
+        testPassed = self.db.getPaperReviewers(1) == expected1
+        # case 2
+        testPassed = self.db.getPaperReviewers(2) == expected1
+        # case 3
+        testPassed = self.db.getPaperReviewers(3) == expected2
+        # case 4
+        testPassed = self.db.getPaperReviewers(4) == expected2       
+        # case 5
+        testPassed = self.db.getPaperReviewers(5) == expected3
+    
+        if testPassed == True:
+            print("Getting paper reviewers has passed")
+        else:
+            print("Getting paper reviewers has failed")
     
     def testGetAllPapers(self):
         expected = [(1, 'Bass players are undervalued big time', 'A', 'DB', 'Harry@Email', 'DB'), (2, 'System of a Down rocks!', 'A', 'SE', 'Harry@Email', 'SE'), (3, 'Three moons are better than one', 'A', 'SE', 'Ron@Email', 'SE'), (4, 'Donald was not always been a duck', 'R', 'SE', 'Hermoine@Email', 'SE'), (5, 'Mickey and Anakin are bothers', 'R', 'SE', 'Snape@Email', 'SE'), (6, 'Previous text here caused an error', 'A', 'DB', 'Harry@Email', 'DB')]
         
-        if self.db.getAllPapers == expected:
+        if self.db.getAllPapers() == expected:
             print("Get all papers has passed")
         else:
             print("Get all papers has failed")
+            
+    def testPotentialReviewers(self):
+        testPassed = True
+        expected6 = [("Mickey@Email",),("Donald@Email",)]
+        expected2 = [("Tom@Email",),("Jerry@Email",),("Anakin@Email",),
+                    ("Darth@Email",),("Pluto@Email",),("R2D2@Email",),
+                    ("C3P0@Email",)]
+        expected3 = [("Tom@Email",),("Jerry@Email",),("Minnie@Email",),("R2D2@Email",),
+                    ("C3P0@Email",)]
+        expected4 = [("Tom@Email",),("Jerry@Email",),("Minnie@Email",),("R2D2@Email",),
+                    ("C3P0@Email",)]
+        expected5 = [("Jerry@Email",),("Anakin@Email",),
+                    ("Darth@Email",),("Pluto@Email",),("Minnie@Email",)]        
+        
+        
+        # case 1
+        testPassed = self.db.getPotentialReviewers(1) == []
+        # case 2
+        testPassed = set(self.db.getPotentialReviewers(2)) == set(expected2)
+        # case 3
+        testPassed = set(self.db.getPotentialReviewers(3)) == set(expected3)
+        # case 4
+        testPassed = set(self.db.getPotentialReviewers(4)) == set(expected4)
+        # case 5
+        testPassed = set(self.db.getPotentialReviewers(5)) == set(expected5)
+        # case 6
+        testPassed = set(self.db.getPotentialReviewers(6)) == set(expected6)
+        
+        if testPassed == True:
+            print("Get Potential Reviewers has passed")
+        else:
+            print("Get Potential Reviewers has failed")
+            
+        
+    def testInsert(self):
+        before = self.db.getAllReviews()
+        self.db.inputReview([3,3,3,3],2,"Minnie@Email")
+        after = self.db.getAllReviews()
+        if after != before:
+            print("Insert passed")
+        else:
+            print("Insert failed")
 
 
 if __name__ == "__main__":
--- source.py
+++ source.py
@@ -6,38 +6,135 @@ import sqlite3
 class UI:
     def __init__(self):
         self.paperNum = 0
+        self.db = Database()
+        
+    def run(self):
+        # main loop
+        while True:
+            # present main screen
+            self.mainScreen()
+            # choose an activity option
+            choice = input("Make a selection: ")
+            if choice == '1':
+                # show user all papers
+                self.viewPapers(self.db.getAllPapers())
+            # exit application
+            elif choice == '6':
+                break
+            else:
+                print("Invalid Selection")
+            
+    
+    def mainScreen(self):
+        # Present all main options to the user
+        
+        options = ["1) See All Papers",
+                   "2) Get Reviewers Within Range of Papers Reviewed",
+                   "3) Sessions Authors Participated In",
+                   "4) View Pie Chart of Top 5 Areas",
+                   "5) View Bar Chart of Average Scores",
+                   "6) Exit"]
+        for option in options:
+            print(option)
     
     def viewPapers(self,papers):
+        # divide all papers into pages
+        # papers is a list of dictionaries
+        
         pages = self.makePages(papers)
         pageCount = len(pages)
         pageIndex = 0
+        # print the first page
         self.printPage(pages[pageIndex])
-        while True:
+        loop = True
+        while loop:
+            # get user input
             key = input("Please choose an action: ")
             print()
+            # present the next page of papers
             if key == 'n':
                 if pageIndex + 1 < pageCount:
                     pageIndex += 1
                 else:
+                    # notify the user they already at the last page
                     print("Already at the last page")
                 self.printPage(pages[pageIndex])
-                
+            # present the user with the previous page of papers
             elif key == 'p':
                 if pageIndex - 1 >= 0:
                     pageIndex -= 1
+                # notify the user they are already on the first page
                 else:
                     print("Already at the first page")
                 self.printPage(pages[pageIndex])
-            elif key >= 1 and key <= self.paperNum:
-                self.showReviewEmails(key)
                 
+            # if the user has choosen a paper, show them further options
+            elif int(key) >= 1 and int(key) <= self.paperNum:
+                loop = False
+                self.decideAllPapers(int(key))
+
                 
-    def showReviewEmails(key):
+    
+    
+    def decideAllPapers(self,key):
+        # print options for the selected paper
+        # key is the paper id
+        
+        options = ["1) See all reviewers",
+                   "2) Add New Review"]
+        for option in options:
+            print(option)
+        # get decision from user and continue accordingly
+        decision = int(input("Make a selection: "))
+        if decision == 1:
+            self.showReviewEmails(key)
+        elif decision == 2:
+            self.showPotentialReviewers(key)
+        
+    
+    def showPotentialReviewers(self,key):
+        # get all potential reviewers
+        # key is the paper id
+        
+        emails = self.db.getPotentialReviewers(key)
+        # notify the user if no potential reviewers exist
+        if not emails:
+            print("No reviewer options available")
+            print()
+            return
+        # present the user with reviewer options
+        options = []
+        for i in range(len(emails)):
+            options.append(str(i+1)+") "+emails[i][0])
+        for option in options:
+            print(option)
+        # continue to add review page
+        choice = int(input("Select a reviewer"))
+        self.addReview(key,emails[choice-1][0])
+        
+    def addReview(self,paper,reviewer):
+        # get all neccessary scores from the user
+        # paper is the paper id
+        # reviewer is the email address of the review
+        
+        scores = []
+        scores.append(int(input("Originality: ")))
+        scores.append(int(input("Importance: ")))
+        scores.append(int(input("Soundness: ")))
+        scores.append(int(input("Overall: ")))
+        # input the score into the database
+        self.db.inputReview(scores,paper,reviewer)
+    
+                
+    def showReviewEmails(self,key):
         pass
     
     
          
     def printPage(self,page):
+        # print requested page
+        # page is a dict of papers
+        
         print("Current papers:")
         for key,paper in page.items():
             print(str(key+1)+") "+paper[1])
@@ -45,6 +142,9 @@ class UI:
                     
             
     def makePages(self,papers):
+        # split all papers into pages of 5
+        # papers is a list of papers
+        
         self.paperNum = len(papers)
         if self.paperNum % 5 == 0:
             pageCount = self.paperNum/5
@@ -74,24 +174,61 @@ class Database:
         self.cursor.close()
         self.connection.close()
         
+    def inputReview(self,scores,paper,reviewer):
+        # add review to the database
+        # scores is the list of scores for the review
+        # paper is the paper id
+        # reviewer is the email address of the reviewer
+        
+        string = '''INSERT INTO reviews
+                    VALUES ({},'{}',{},{},{},{})'''.format(paper,reviewer,scores[0],
+                                                         scores[1],scores[2],scores[3])
+        self.cursor.execute(string)
+        self.connection.commit()
+        
     def getAllPapers(self):
+        # get all papers in the database
+        
         self.cursor.execute("SELECT * FROM papers;")
         papers = self.cursor.fetchall()
         return papers
     
     def getPaperReviewers(self,pID):
-        pID = (pID)
-        self.cursor.execute
-        ('''SELECT reviewer
-            FROM reviews
-            WHERE paper=:id
-            GROUP BY reviewer,paper;''',{"id":pID})
+        # get reviewers for a certain paper
+        # pID is the paper id
+        
+        self.cursor.execute("SELECT reviewer FROM reviews WHERE paper=:id;",{"id":pID})
+        emails = self.cursor.fetchall() 
+        return emails
+    
+    def getPotentialReviewers(self,pID):
+        # get all potential reviewers for a certain paper
+        # pID is the paper id
+        
+        string = '''SELECT e.reviewer
+                    FROM papers p, expertise e
+                    WHERE p.area = e.area and p.Id = {}
+                    EXCEPT
+                    SELECT r.reviewer
+                    FROM reviews r
+                    WHERE r.paper = {}'''.format(pID,pID) 
+        self.cursor.execute(string)
         emails = self.cursor.fetchall()
         return emails
+    
+    def getAllReviews(self):
+        # get all reviews
+        # for testing purposes
+        
+        self.cursor.execute("SELECT * FROM reviews")
+        reviews = self.cursor.fetchall()
+        return reviews
+    
+        
         
         
     
 if __name__ == "__main__":
     userUI = UI()
-    db = Database()
-    userUI.viewPapers(db.getAllPapers())
+    userUI.run()
+    #userUI.viewPapers(db.getAllPapers())
